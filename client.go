b1cc38e628ceaa7d4c5589956aafaa1736e87cb7
Radek Benkel <218345+singles@users.noreply.github.com>
2022-03-01 21:34:20 +0100

    Exract browser logic to `client.go`

---
+++ client.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"github.com/headzoo/surf/browser"
+	"gopkg.in/headzoo/surf.v1"
+)
+
+type Address struct {
+	Email   string `json:"email"`
+	Memo    string `json:"memo"`
+	Expires string `json:"expires"`
+}
+
+type Client struct {
+	browser *browser.Browser
+}
+
+func NewClient(username, password string) *Client {
+	client := &Client{browser: surf.NewBrowser()}
+
+	err := client.browser.Open("https://manage.mailbox.org/login.php?redirect=account_disposable_aliases")
+	if err != nil {
+		panic(err)
+	}
+
+	fm, _ := client.browser.Form("#io-ox-login-form")
+	fm.Input("username", username)
+	fm.Input("password", password)
+	if fm.Submit() != nil {
+		panic(err)
+	}
+
+	return client
+}
+
+func (c *Client) List() []Address {
+	addresses := []Address{}
+
+	c.browser.Find(".ox-list li").Each(func(_ int, s *goquery.Selection) {
+		email := s.Find(".title div").Text()
+		memo := s.Find(".memo #memo").AttrOr("value", "<no memo>")
+		expires := s.Find(".content div").Text()
+
+		addresses = append(addresses, Address{
+			Email:   email,
+			Memo:    memo,
+			Expires: expires,
+		})
+	})
+
+	return addresses
+}
--- main.go
+++ main.go
@@ -7,16 +7,7 @@ import (
 	"fmt"
 	"io"
 	"os"
-
-	"github.com/PuerkitoBio/goquery"
-	"gopkg.in/headzoo/surf.v1"
 )
-
-type Address struct {
-	Email   string `json:"email"`
-	Memo    string `json:"memo"`
-	Expires string `json:"expires"`
-}
 
 func main() {
 	username := flag.String("username", "", "mailbox.org username")
@@ -30,34 +21,9 @@ func main() {
 
 	password := readPasswordFromStdin(os.Stdin)
 
-	bow := surf.NewBrowser()
-	err := bow.Open("https://manage.mailbox.org/login.php?redirect=account_disposable_aliases")
-	if err != nil {
-		panic(err)
-	}
+	client := NewClient(*username, password)
 
-	fm, _ := bow.Form("#io-ox-login-form")
-	fm.Input("username", *username)
-	fm.Input("password", password)
-	if fm.Submit() != nil {
-		panic(err)
-	}
-
-	addresses := []Address{}
-
-	bow.Find(".ox-list li").Each(func(_ int, s *goquery.Selection) {
-		email := s.Find(".title div").Text()
-		memo := s.Find(".memo #memo").AttrOr("value", "<no memo>")
-		expires := s.Find(".content div").Text()
-
-		addresses = append(addresses, Address{
-			Email:   email,
-			Memo:    memo,
-			Expires: expires,
-		})
-	})
-
-	addressesJson, err := json.MarshalIndent(addresses, "", "  ")
+	addressesJson, err := json.MarshalIndent(client.List(), "", "  ")
 
 	if err != nil {
 		fmt.Println(err)
