a045a6ebc57e79db783761f0320f8b2c48bcc56a
Radek Benkel <218345+singles@users.noreply.github.com>
2022-03-03 23:24:57 +0100

    Add CLI configuration

--- go.mod
+++ go.mod
@@ -4,12 +4,14 @@ go 1.17
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/alexflint/go-arg v1.4.3
+	github.com/headzoo/surf v1.0.1
 	gopkg.in/headzoo/surf.v1 v1.0.1
 )
 
 require (
+	github.com/alexflint/go-scalar v1.1.0 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/headzoo/surf v1.0.1 // indirect
 	github.com/headzoo/ut v0.0.0-20181013193318-a13b5a7a02ca // indirect
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 )
--- main.go
+++ main.go
@@ -2,35 +2,71 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
-	"flag"
 	"fmt"
 	"io"
 	"os"
+
+	"github.com/alexflint/go-arg"
 )
 
-func main() {
-	username := flag.String("username", "", "mailbox.org username")
-	flag.Parse()
+type ListCommand struct{}
 
-	if *username == "" {
-		fmt.Println("Usage: command-that-passess | mailbox-org-cli --username user@example.com")
-		flag.PrintDefaults()
+type RenewCommand struct {
+	ID string `arg:"required"`
+}
+
+type DeleteCommand struct {
+	ID string `arg:"required"`
+}
+
+type SetMemoCommand struct {
+	ID   string `arg:"required"`
+	Memo string `arg:"required"`
+}
+
+type CreateCommand struct {
+	Memo string
+}
+
+type args struct {
+	Username string          `arg:"required" help:"mailbox.org username"`
+	List     *ListCommand    `arg:"subcommand:list" help:"list dispossable addresses"`
+	Renew    *RenewCommand   `arg:"subcommand:renew" help:"renew dispossable address"`
+	Delete   *DeleteCommand  `arg:"subcommand:delete" help:"delete dispossable address"`
+	SetMemo  *SetMemoCommand `arg:"subcommand:set-memo" help:"set-memo on existing dispossable address"`
+	Create   *CreateCommand  `arg:"subcommand:create" help:"create new dispossable address with optional memo"`
+}
+
+func (args) Description() string {
+	return "Commad line \"client\" for mailbox.org dispossable addresses feature"
+}
+
+func main() {
+	var args args
+	p := arg.MustParse(&args)
+
+	stdin := os.Stdin
+	stat, _ := stdin.Stat()
+
+	if (stat.Mode() & os.ModeCharDevice) != 0 {
+		fmt.Println("You must pipe password into this command, exiting.")
 		os.Exit(1)
 	}
 
-	password := readPasswordFromStdin(os.Stdin)
-
-	client := NewClient(*username, password)
-
-	data := client.Extend("id")
-	output, err := json.MarshalIndent(data, "", "  ")
-
-	if err != nil {
-		fmt.Println(err)
+	switch {
+	case args.List != nil:
+		fmt.Println("listing addressess")
+	case args.Renew != nil:
+		fmt.Println("Renewing address")
+	case args.Delete != nil:
+		fmt.Println("Creating address")
+	case args.Create != nil:
+		fmt.Println("Adding disposable address")
+	case args.SetMemo != nil:
+		fmt.Println("Setting memo")
+	default:
+		p.Fail("Invalid command")
 	}
-
-	fmt.Println(string(output))
 }
 
 func readPasswordFromStdin(stdin io.Reader) string {
